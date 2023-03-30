8345ac80c0e1dfe5f915e6b17633946a9ce14ce0
bahati <bulozealfani@gmail.com>
2023-03-29 21:44:45 -0500

    checking if there's a new tour and sending email

--- main.py
+++ main.py
@@ -18,3 +18,15 @@ body = "New tour dates have been announced on the band's website: " + url
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 tour_dates = soup.find_all("div", {"class": "tour-date"})
+
+# Check if there are new tour dates
+if len(tour_dates) > 0:
+    # Send email notification
+    with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+        smtp.starttls()
+        smtp.login(sender_email, sender_password)
+        message = f"Subject: {subject}\n\n{body}"
+        smtp.sendmail(sender_email, recipient_email, message.encode("utf-8"))
+        print("Email sent.")
+else:
+    print("No new tour dates found.")
