84b1f5c52bf8c609bb89e16fe73b78448f01dcf2
bahati <bulozealfani@gmail.com>
2023-03-29 21:43:27 -0500

    adding sender's email and password and getting html content from web URL

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ import smtplib
 
 # Website URL to scrape for tour dates
 url = "https://www.example.com/tours"
+
+# Email parameters
+sender_email = "your_email@example.com"
+sender_password = "your_email_password"
+recipient_email = "recipient_email@example.com"
+
+# Email message
+subject = "New tour dates available"
+body = "New tour dates have been announced on the band's website: " + url
+
+# Scrape website for tour dates
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+tour_dates = soup.find_all("div", {"class": "tour-date"})
